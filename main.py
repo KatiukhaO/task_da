dfd4af1ae8963471fbd67125f66dacb94f681e15
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-08-02 19:40:31 +0300

    Solution

---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+from datetime import timedelta
+
+import pandas as pandas
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster
+from couchbase.options import ClusterOptions, ClusterTimeoutOptions, QueryOptions
+
+
+endpoint = "cb.jwehljizv78ywblm.cloud.couchbase.com"
+username = "1109katiuha@gmail.com"
+password = os.environ.get("PASSWORD")
+bucket_name = "travel-sample"
+
+auth = PasswordAuthenticator(username, password)
+
+timeout_opts = ClusterTimeoutOptions(kv_timeout=timedelta(seconds=10))
+
+cluster = Cluster('couchbases://{}'.format(endpoint),
+                  ClusterOptions(auth, timeout_options=timeout_opts))
+
+cluster.wait_until_ready(timedelta(seconds=5))
+
+
+def load_data():
+    print("\nLoad data ...")
+    try:
+        sql_query = "SELECT * FROM `travel-sample`.inventory.airline"
+        row_iter = cluster.query(sql_query)
+        return row_iter
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+
+    travel_data = load_data()
+
+    df_ = pandas.DataFrame(travel_data)
+
+    data_dict = {key : [] for row in df_.values for key in row[0]}
+
+    for row in df_.values:
+        for key in data_dict.keys():
+            if data_dict.keys() == row[0].keys():
+                data_dict[key].append(row[0][key])
+            else:
+                data_dict[key].append(None)
+
+    df1 = pandas.DataFrame(data_dict)
+    df1.to_csv(r'C:\Users\1109k\Desktop\test_travel-sample_data.csv')
+    print("CSV file was create success")
+
+    df2 = df1.copy()
+    df2["textColumn"] = "test"
+
+    df1.merge(df2, how="cross")
+    print("Merge ...")
+    print(df1)
+
+
+
